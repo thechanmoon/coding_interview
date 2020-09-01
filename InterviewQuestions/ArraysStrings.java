451477aa5dc7d2aa009d6d4979a1b13c206ed73e
chanmoonpark <thechanmoon@gmail.com>
2020-09-01 12:50:14 -0500

    ArrayList<String> merge() test

--- InterviewQuestions/ArraysStrings.java
+++ InterviewQuestions/ArraysStrings.java
@@ -1,9 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ArraysStrings {
+
+    static ArrayList<String> merge(final String[] words, final String[] more) {
+        final ArrayList<String> sentence = new ArrayList<String>();
+        for (final String w : words)
+            sentence.add(w);
+        for (final String w : more)
+            sentence.add(w);
+        return sentence;
+    }
+
     // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique
     // characters. What if you cannot use additional data structures?
     static boolean isUnique(final String str) {
@@ -20,13 +31,13 @@ public class ArraysStrings {
         return true;
     }
 
-    static boolean isUniqueChar(String str) {
+    static boolean isUniqueChar(final String str) {
         final int ARRAY_LENGTH = 128;
 
         if (str.length() > ARRAY_LENGTH)
             return false;
 
-        boolean[] bAsciiTable = new boolean[ARRAY_LENGTH];
+        final boolean[] bAsciiTable = new boolean[ARRAY_LENGTH];
         int asciiCode = 0;
         for (int index = 0; index < str.length(); index++) {
             asciiCode = str.charAt(index);
@@ -38,7 +49,23 @@ public class ArraysStrings {
         return true;
     }
 
+    // 1.2 Check Permutation: Given two strings, write a method to decide if one is
+    // a permutation of the other.
+    // static boolean isPermutation(final String strA, final String strB)
+    // {
+    // if(strA.length() != strB.length())
+    // return false;
+
+    // final Char[] strArrayA = strA.to
+
+    // return true;
+    // }
+
     public static void main(final String[] args) {
+
+        // merge
+        // String[] words = { "I", "Love", "You", "!!" };
+        // System.out.println(merge(words, words));
 
         // 1.1 Is Unique: Implement an algorithm to determine if a string has all unique
         // characters. What if you cannot use additional data structures?
@@ -60,16 +87,16 @@ public class ArraysStrings {
         // }
         // System.out.println(a);
 
-        Scanner sc = new Scanner(System.in);
-        while (!sc.hasNext("exit")) {
-            // System.out.println("isUnique(a) : " + isUnique(sc.next()));
-            System.out.println("isUniqueChar(a) : " + isUniqueChar(sc.next()));
-            // System.out.println(sc.hasNextInt() ? "(int) " + sc.nextInt()
-            // : sc.hasNextLong() ? "(long) " + sc.nextLong()
-            // : sc.hasNextDouble() ? "(double) " + sc.nextDouble()
-            // : sc.hasNextBoolean() ? "(boolean) " + sc.nextBoolean() : "(String) " +
-            // sc.next());
-        }
+        // Scanner sc = new Scanner(System.in);
+        // while (!sc.hasNext("exit")) {
+        // // System.out.println("isUnique(a) : " + isUnique(sc.next()));
+        // System.out.println("isUniqueChar(a) : " + isUniqueChar(sc.next()));
+        // // System.out.println(sc.hasNextInt() ? "(int) " + sc.nextInt()
+        // // : sc.hasNextLong() ? "(long) " + sc.nextLong()
+        // // : sc.hasNextDouble() ? "(double) " + sc.nextDouble()
+        // // : sc.hasNextBoolean() ? "(boolean) " + sc.nextBoolean() : "(String) " +
+        // // sc.next());
+        // }
 
     }
 }
