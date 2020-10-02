0549dfa9e662d1215bdf5d68f6d9878aa7d271e9
chanmoonpark <thechanmoon@gmail.com>
2020-10-02 10:45:43 -0500

    Memoization Fibonachi Recursive added

--- InterviewQuestions/Memoization.java
+++ InterviewQuestions/Memoization.java
@@ -1,4 +1,19 @@
 public class Memoization {
+    public static int fibonachi_recursive(int n) {
+
+        if (n < 0) {
+            throw new IllegalArgumentException("Index was negative. No such thing as a negative index in a series.");
+        }
+
+        // base cases
+        if (n == 0 || n == 1) {
+            return n;
+        }
+
+        System.out.printf("computing fib(%d)\n", n);
+        return fibonachi_recursive(n - 1) + fibonachi_recursive(n - 2);
+    }
+
     public static int fibonachi(int n, int[] r) {
         if (n < 0)
             return 0;
@@ -14,7 +29,10 @@ public class Memoization {
         // System.out.println(fibonachi(3, array));
         // System.out.println(fibonachi(4, array));
         // System.out.println(fibonachi(5, array));
-        for (int i = 0; i < array.length; i++)
-            System.out.println(fibonachi(i, array));
+
+        // for (int i = 0; i < array.length; i++)
+        // System.out.println(fibonachi(i, array));
+
+        System.out.println(fibonachi_recursive(10));
     }
 }
