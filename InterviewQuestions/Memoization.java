7ce3667fe80745ed56849ab37cff30b3b8f51e14
chanmoonpark <thechanmoon@gmail.com>
2020-10-02 10:52:35 -0500

    Memoization Fibonach Hashmap version added

--- InterviewQuestions/Memoization.java
+++ InterviewQuestions/Memoization.java
@@ -1,4 +1,34 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Memoization {
+    public static Map<Integer, Integer> memo = new HashMap<>();
+
+    public static int fibonach_hashmap(int n) {
+
+        if (n < 0) {
+            throw new IllegalArgumentException("Index was negative. No such thing as a negative index in a series.");
+
+            // base cases
+        } else if (n == 0 || n == 1) {
+            return n;
+        }
+
+        // see if we've already calculated this
+        if (memo.containsKey(n)) {
+            System.out.printf("grabbing memo[%d]\n", n);
+            return memo.get(n);
+        }
+
+        System.out.printf("computing fib(%d)\n", n);
+        int result = fibonach_hashmap(n - 1) + fibonach_hashmap(n - 2);
+
+        // memoize
+        memo.put(n, result);
+
+        return result;
+    }
+
     public static int fibonachi_recursive(int n) {
 
         if (n < 0) {
@@ -10,14 +40,14 @@ public class Memoization {
             return n;
         }
 
-        System.out.printf("computing fib(%d)\n", n);
+        // System.out.printf("computing fib(%d)\n", n);
         return fibonachi_recursive(n - 1) + fibonachi_recursive(n - 2);
     }
 
     public static int fibonachi(int n, int[] r) {
-        if (n < 0)
-            return 0;
-        else if (n < 2)
+        if (n < 0) {
+            throw new IllegalArgumentException("Index was negative. No such thing as a negative index in a series.");
+        } else if (n < 2)
             return r[n] = 1;
         else
             return r[n] = fibonachi(n - 1, r) + fibonachi(n - 2, r);
@@ -33,6 +63,7 @@ public class Memoization {
         // for (int i = 0; i < array.length; i++)
         // System.out.println(fibonachi(i, array));
 
-        System.out.println(fibonachi_recursive(10));
+        // System.out.println(fibonachi_recursive(10));
+        System.out.println(fibonach_hashmap(10));
     }
 }
